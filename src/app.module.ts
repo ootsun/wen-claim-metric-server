63517b9d18c8f6c3fdb50132f863b5d48a4032ac
ootsun <gabriel.delhaye@hotmail.com>
2022-01-21 12:02:04 +0100

    Fix Typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {Metric} from './metric/metric.entity';
 import {MetricModule} from './metric/metric.module';
 
+let connectionString = process.env.DB_PROTOCOL + "://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST;
+if(Number.parseInt(process.env.DB_PORT) > 0) {
+  connectionString += ":" + process.env.DB_PORT;
+}
+connectionString += "/" + process.env.DB_DATABASE_NAME;
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      host: process.env.DB_HOST,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE_NAME,
+      url: connectionString,
       entities: [Metric],
       synchronize: true,
     }),
