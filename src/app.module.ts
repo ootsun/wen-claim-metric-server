57a49faf27031c2a2e3f9fd0275bcf456d0cf099
ootsun <gabriel.delhaye@hotmail.com>
2022-01-21 19:22:26 +0100

    Fix UserRepository

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import {Metric} from './metric/metric.entity';
 import {MetricModule} from './metric/metric.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import {User} from './users/user.entity';
 
 let connectionString = process.env.DB_PROTOCOL + "://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST;
 if(Number.parseInt(process.env.DB_PORT) > 0) {
@@ -17,7 +18,7 @@ connectionString += "/" + process.env.DB_DATABASE_NAME;
       type: 'mongodb',
       url: connectionString,
       useUnifiedTopology: true,
-      entities: [Metric],
+      entities: [Metric, User],
       synchronize: true,
     }),
     MetricModule,
