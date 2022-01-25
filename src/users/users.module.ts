8ac5dff82169483119c3890dcd405b06d7e55b68
ootsun <gabriel.delhaye@hotmail.com>
2022-01-25 17:35:48 +0100

    Import AuthModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import {UserService} from './user.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {User} from './user.entity';
 import {UserController} from './user.controller';
+import {AuthModule} from '../auth/auth.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User])
+        TypeOrmModule.forFeature([User]),
+        AuthModule
     ],
     providers: [
         UserService
